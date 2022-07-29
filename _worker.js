5836c9f3991c964571e1fd15acac0f089240aead
Xin <39113904+XinXinQAQ@users.noreply.github.com>
2022-07-29 12:00:59 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,12 +1,21 @@
 export default {
-    async fetch(request, env) {
-      let url = new URL(request.url);
-      if (url.pathname.startsWith('/')) {
-        url.hostname="ppclub.ml";
-        let new_request=new Request(url,request);
-        return fetch(new_request);
-      }
-      // Otherwise, serve the static assets.
-      return env.ASSETS.fetch(request);
-    }
-  };
+	async fetch(request, env) {
+		const cars = [
+			"ppclub.ml", 
+			"blog.ppclub.ml", 
+			"ql.ppclub.ml", 
+			"bbs.ppclub.ml", 
+			"pan.ppclub.ml"
+		];
+		let host = cars[Math.floor(Math.random() * cars.length)];	//随机选择VPS
+		//let host = cars[new Date().getDate() % cars.length];		//每天自动更换VPS
+		
+		let url = new URL(request.url);
+		if (url.pathname.startsWith('/')) {
+			url.hostname = host;
+			let new_request = new Request(url,request);
+			return fetch(new_request);
+		}
+		return env.ASSETS.fetch(request);
+	}
+};
